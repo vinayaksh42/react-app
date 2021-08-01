28079ed991ea461f36a0f7b02f80f937a886a661
vinayaksh42 <vinayaksh42@gmail.com>
2021-08-01 10:49:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import { EOS_STAR } from 'eos-icons-react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <EOS_STAR size="xxxl"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
